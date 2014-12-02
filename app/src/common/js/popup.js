edf2a20906f4ab20ef6db856e21ae19a339d98c0
Seldszar <0xfuture@gmail.com>
2014-12-02 19:06:04 +0100

    Refactorisation pour la prochaine intégration de JSHint Ajout d'un bouton de partage par chaîne Suppression du thème sombre

--- app/src/common/js/popup.js
+++ app/src/common/js/popup.js
@@ -1,140 +1,150 @@
-angular.module('popup', [
-    'ui.router'
-]).
-config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|chrome-extension):/);
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data-image\/)/);
-}]).
-config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/channels');
-    $stateProvider.
-        state('channels', {
-            url: '/channels',
-            views: {
-                '@': {
-                    templateUrl: 'partials/channels.html',
-                    controller: ['$rootScope', '$scope', '$stateParams', function ($rootScope, $scope, $stateParams) {
-                        $scope.channels = [];
-                        $scope.refresh = function () {
-                            kango.invokeAsync('extension.getChannels', function (channels) {
-                                $scope.$apply(function () {
-                                    $scope.channels = channels;
+(function () {
+    'use strict';
+
+    angular.module('popup', [
+        'ui.router'
+    ]).
+        config(['$compileProvider', function ($compileProvider) {
+            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|chrome-extension):/);
+            $compileProvider.imgSrcSanitizationWhitelist(/^\s*((https?|ftp|file|blob|chrome-extension):|data-image\/)/);
+        }]).
+        config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/channels');
+            $stateProvider.
+                state('channels', {
+                    url: '/channels',
+                    views: {
+                        '@': {
+                            templateUrl: 'partials/channels.html',
+                            controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
+                                $scope.channels = [];
+                                kango.invokeAsync('extension.getChannels', function (channels) {
+                                    $scope.$apply(function () {
+                                        $scope.channels = channels;
+                                    });
                                 });
-                            });
-                        };
-                        $scope.toggleFavorite = function (broadcast, event) {
-                            event.stopImmediatePropagation();
-                            kango.invokeAsync('extension.toggleFavorite', broadcast.id_contenu, function (isFavorite) {
-                                $scope.$apply(function () {
-                                    broadcast.is_favorite = isFavorite;
+                                $scope.favorite = function (broadcast) {
+                                    kango.invokeAsync('extension.toggleFavorite', broadcast.id_contenu, function (isFavorite) {
+                                        $scope.$apply(function () {
+                                            broadcast.is_favorite = isFavorite;
+                                        });
+                                    });
+                                };
+                                $scope.share = function (broadcast) {
+                                    kango.browser.tabs.create({
+                                        url: 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('Je regarde actuellement ' + broadcast.titre + ' sur Gaming Live') + '&url=' + encodeURIComponent('http://www.jeuxvideo.com' + broadcast.url)
+                                    });
+                                };
+                            }]
+                        }
+                    }
+                }).
+                state('schedule', {
+                    url: '/schedule',
+                    abstract: true,
+                    views: {
+                        'sub-menu': {
+                            templateUrl: 'partials/schedule.sub-menu.html',
+                            controller: ['$scope', function ($scope) {
+                                $scope.channels = [];
+                                kango.invokeAsync('extension.getChannels', function (channels) {
+                                    $scope.$apply(function () {
+                                        $scope.channels = channels;
+                                    });
                                 });
-                            });
-                        };
-                        $scope.refresh();
-                    }]
-                }
-            }
-        }).
-        state('schedule', {
-            url: '/schedule',
-            abstract: true,
-            views: {
-                'sub-menu': {
-                    templateUrl: 'partials/schedule.sub-menu.html',
-                    controller: ['$scope', function ($scope) {
-                        $scope.channels = [];
-                        $scope.refresh = function () {
-                            kango.invokeAsync('extension.getChannels', function (channels) {
-                                $scope.$apply(function () {
-                                    $scope.channels = channels;
+                            }]
+                        }
+                    }
+                }).
+                state('schedule.show', {
+                    url: '/show/:id',
+                    views: {
+                        '@': {
+                            templateUrl: 'partials/schedule.show.html',
+                            controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
+                                $scope.schedule = [];
+                                kango.invokeAsync('extension.getSchedule', $stateParams.id, function (schedule) {
+                                    $scope.$apply(function () {
+                                        $scope.schedule = schedule;
+                                    });
                                 });
-                            });
-                        };
-                        $scope.refresh();
-                    }]
+                            }]
+                        }
+                    }
+                }).
+                state('infos', {
+                    url: '/infos',
+                    abstract: true,
+                    views: {
+                        'sub-menu': {
+                            templateUrl: 'partials/infos.sub-menu.html'
+                        }
+                    }
+                }).
+                state('infos.about', {
+                    url: '/about',
+                    views: {
+                        '@': {
+                            templateUrl: 'partials/infos.about.html'
+                        }
+                    }
+                }).
+                state('infos.settings', {
+                    url: '/settings',
+                    views: {
+                        '@': {
+                            templateUrl: 'partials/infos.settings.html'
+                        }
+                    }
+                });
+        }]).
+        directive('openTab', function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attr) {
+                    element.on('click', function (event) {
+                        event.preventDefault();
+                        kango.browser.tabs.create({
+                            url: attr.openTab || attr.href
+                        });
+                    });
                 }
-            }
+            };
         }).
-        state('schedule.show', {
-            url: '/show/:id',
-            views: {
-                '@': {
-                    templateUrl: 'partials/schedule.show.html',
-                    controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
-                        $scope.schedule = [];
-                        $scope.refresh = function () {
-                            kango.invokeAsync('extension.getSchedule', $stateParams.id, function (schedule) {
-                                $scope.$apply(function () {
-                                    $scope.schedule = schedule;
-                                });
-                            });
-                        };
-                        $scope.refresh();
-                    }]
+        directive('openTabIgnore', function () {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attr) {
+                    element.on('click', function (event) {
+                        event.stopImmediatePropagation();
+                    });
                 }
-            }
+            };
         }).
-        state('infos', {
-            url: '/infos',
-            abstract: true,
-            views: {
-                'sub-menu': {
-                    templateUrl: 'partials/infos.sub-menu.html'
-                }
-            }
+        directive('extensionVersion', function () {
+            return {
+                restrict: 'A',
+                template: kango.getExtensionInfo().version
+            };
         }).
-        state('infos.about', {
-            url: '/about',
-            views: {
-                '@': {
-                    templateUrl: 'partials/infos.about.html'
-                }
-            }
-        }).
-        state('infos.settings', {
-            url: '/settings',
-            views: {
-                '@': {
-                    templateUrl: 'partials/infos.settings.html'
-                }
-            }
-        });
-}]).
-directive('openTab', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attr) {
-            element.on('click', function (event) {
-                event.preventDefault();
-                kango.browser.tabs.create({
-                    url: attr.openTab || attr.href
+        run(['$rootScope', '$state', function ($rootScope, $state) {
+            $rootScope.$state = $state;
+            $rootScope.settings = {};
+            kango.invokeAsync('extension.getSettings', function (settings) {
+                $rootScope.$apply(function () {
+                    $rootScope.settings = settings;
+                    $rootScope.$watch('settings', function (newVal) {
+                        kango.invokeAsync('extension.setSettings', newVal);
+                    }, true);
                 });
             });
-        }
-    }
-}).
-directive('extensionVersion', function () {
-    return {
-        restrict: 'A',
-        template: kango.getExtensionInfo().version
-    }
-}).
-run(['$rootScope', '$state', function ($rootScope, $state) {
-    $rootScope.$state = $state;
-    $rootScope.settings = {};
-    kango.invokeAsync('extension.getSettings', function (settings) {
-        $rootScope.$apply(function () {
-            $rootScope.settings = settings;
-            $rootScope.$watch('settings', function (newVal) {
-                kango.invokeAsync('extension.setSettings', newVal);
-            }, true);
-        });
-    });
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        ga('send', 'pageview', $state.href(toState, toParams));
-    });
-}]);
+            $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+                ga('send', 'pageview', $state.href(toState, toParams));
+            });
+        }]);
 
-KangoAPI.onReady(function () {
-    angular.bootstrap(document, ['popup']);
-});
+    KangoAPI.onReady(function () {
+        angular.bootstrap(document, ['popup']);
+    });
+
+}());
